736e8d11dedea918ed4164eec47a9da06094d682
Abhishekjha20 <jha202001@gmail.com>
2024-10-19 23:18:43 +0530

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,15 @@
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+        {/* Logo and Site name */}
+        <div>
+        </div>
+         {/*List of other tabs */}
+         <ul>
+
+         </ul>
+    </div>
   )
 }
 
