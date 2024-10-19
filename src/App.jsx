9a1fee80fb36f6c98a3766cafd4d31dabb353f1d
Abhishekjha20 <jha202001@gmail.com>
2024-10-19 16:03:32 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
-import './App.css'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import SellerDashboard from "./pages/SellerDashboard";
+import BuyerDashboard from "./pages/BuyerDashboard";
+
 
 function App() {
  
 
   return (
     <>
-      <p>Hello</p>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element = {<Login/>}/>
+        <Route path="/signup" element = {<Signup/>}/>
+        <Route path = "/seller/profile" element = {<SellerDashboard/>}/>
+        <Route path = "buyer/profile" element = {<BuyerDashboard/>}/>
+      </Routes>
+      </BrowserRouter> 
     </>
-  )
+  ) 
 }
 
 export default App
